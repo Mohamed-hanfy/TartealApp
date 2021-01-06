9da2d8626e9c5e81e7dbe8afd789702b02e175db
medo-medo15 <66412159+medo-medo15@users.noreply.github.com>
2021-01-06 19:53:48 +0200

    VLC Player

--- Tarteal/Views/MotaMalekPrograme/MoataaMaster.xaml.cs
+++ Tarteal/Views/MotaMalekPrograme/MoataaMaster.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibVLCSharp.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,17 @@ namespace Tarteal.Views.MotaMalekPrograme
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MoataaMaster : ContentPage
     {
+        LibVLC _libvlc;
         public MoataaMaster(Quran qaaaa)
         {
             BindingContext = qaaaa;
             InitializeComponent();
+            Core.Initialize();
+            _libvlc = new LibVLC();
+            var media = new Media(_libvlc, qaaaa.URL,
+            FromType.FromLocation);
+            myVideo.MediaPlayer = new MediaPlayer(media) { EnableHardwareDecoding = true };
+            myVideo.MediaPlayer.Play();
         }
     }
 }
